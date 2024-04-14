ff2b67b20c3f685cddfd18ab62ce7ec632c9b2a4
harshitab07 <harshitabhatt1730@gmail.com>
2024-04-14 22:16:33 +0530

    google analytics

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HomeData from '../data';
 import heroImg from "../assets/heroImg.webp";
 import axios from 'axios';
